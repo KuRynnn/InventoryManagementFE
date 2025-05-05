89d5f9c1b56256da4a3057505235efe337fbde05
KuRynnn <fahryan107@gmail.com>
2025-05-05 11:47:28 +0700

    fix http link

--- src/app/components/ItemListWrapper.tsx
+++ src/app/components/ItemListWrapper.tsx
@@ -7,7 +7,7 @@ const ItemListWrapper = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/items')
+    fetch('http://103.87.66.188:3000/api/items')
       .then(response => response.json())
       .then(data => setItems(data))
       .catch(error => console.error('Error fetching items:', error));
--- src/app/components/TransactionForm.tsx
+++ src/app/components/TransactionForm.tsx
@@ -41,7 +41,7 @@ const TransactionForm: React.FC<TransactionFormProps> = ({ onSubmit, isLoading, 
   useEffect(() => {
     const loadItems = async () => {
       try {
-        const data = await fetchData('http://localhost:3000/api/items');
+        const data = await fetchData('http://103.87.66.188:3000/api/items');
         setItems(data);
       } catch (error) {
         console.error('Error fetching items:', error);
